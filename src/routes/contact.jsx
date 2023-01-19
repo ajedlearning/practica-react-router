1c39841b4a15fc0a2a6241a8edeedfcd63790a76
Arnaldo <arnaldoespinoza1@hotmail.com>
2023-01-19 16:15:43 -0400

    finalizacion del tuto de la documentacionde reactrouter

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,13 +1,26 @@
+import { Form, useLoaderData, useFetcher } from 'react-router-dom'
 
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
-
+import { getContact, updateContact } from '../contacts'
 export async function loader({ params }) {
-  return getContact(params.contactId);
+  const contact = await getContact(params.contactId)
+  if (!contact) {
+    throw new Response('', {
+      status: 404,
+      statusText: 'Not Found',
+    })
+  }
+  return contact
+}
+
+export async function action({ request, params }) {
+  let formData = await request.formData()
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true',
+  })
 }
 
 export default function Contact() {
-  const contact = useLoaderData();
+  const contact = useLoaderData()
   // const contact = {
   //   first: "Your",
   //   last: "Name",
@@ -20,10 +33,7 @@ export default function Contact() {
   return (
     <div id="contact">
       <div>
-        <img
-          key={contact.avatar}
-          src={contact.avatar || null}
-        />
+        <img key={contact.avatar} src={contact.avatar || null} />
       </div>
 
       <div>
@@ -34,16 +44,13 @@ export default function Contact() {
             </>
           ) : (
             <i>No Name</i>
-          )}{" "}
+          )}{' '}
           <Favorite contact={contact} />
         </h1>
 
         {contact.twitter && (
           <p>
-            <a
-              target="_blank"
-              href={`https://twitter.com/${contact.twitter}`}
-            >
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
               {contact.twitter}
             </a>
           </p>
@@ -59,12 +66,8 @@ export default function Contact() {
             method="post"
             action="destroy"
             onSubmit={(event) => {
-              if (
-                !confirm(
-                  "Please confirm you want to delete this record."
-                )
-              ) {
-                event.preventDefault();
+              if (!confirm('Please confirm you want to delete this record.')) {
+                event.preventDefault()
               }
             }}
           >
@@ -73,25 +76,25 @@ export default function Contact() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 function Favorite({ contact }) {
   // yes, this is a `let` for later
-  let favorite = contact.favorite;
+  const fetcher = useFetcher()
+  let favorite = contact.favorite
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true'
+  }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
-        value={favorite ? "false" : "true"}
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
+        value={favorite ? 'false' : 'true'}
+        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
       >
-        {favorite ? "★" : "☆"}
+        {favorite ? '★' : '☆'}
       </button>
-    </Form>
-  );
+    </fetcher.Form>
+  )
 }
--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,7 +1,18 @@
-import { Form, useLoaderData } from 'react-router-dom'
+import { Form, useLoaderData, redirect, useNavigate } from 'react-router-dom'
+import { updateContact } from '../contacts'
+
+export async function action({ request, params }) {
+  const formData = await request.formData()
+  // const firstName = formData.get("first");
+  // const lastName = formData.get("last");
+  const updates = Object.fromEntries(formData)
+  await updateContact(params.contactId, updates)
+  return redirect(`/contacts/${params.contactId}`)
+}
 
 export default function EditContact() {
   const contact = useLoaderData()
+  const navigate = useNavigate()
 
   return (
     <Form method="post" id="contact-form">
@@ -47,7 +58,14 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1)
+          }}
+        >
+          Cancel
+        </button>
       </p>
     </Form>
   )
