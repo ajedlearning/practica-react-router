96536e3344284b4ff69a7fb6b08828bcd04e5af7
Arnaldo J. Espinoza D <arnaldoespinoza1@hotmail.com>
2023-01-19 21:52:22 -0400

    Se eliminaron dos linea que mostraban error al intentar eliminar

--- src/main.jsx
+++ src/main.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
+       // errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
